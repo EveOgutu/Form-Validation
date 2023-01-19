75518ceeec4ee07957932f8ec3c15f733a06224b
EveOgutu <120215430+EveOgutu@users.noreply.github.com>
2023-01-19 12:29:52 +0300

    Edited script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function showError(input, message) {
 //Show success outline
 function showSuccess(input) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control success;'
+    formControl.className = 'form-control success';
 }
 
 //Check email validity
